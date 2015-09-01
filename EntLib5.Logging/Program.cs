1faf3ea7ca7e5dc032a445a2ea6300656132658d
YuanRui <yuanrui@live.cn>
2015-09-01 13:44:38 +0800

    first commit

---
+++ EntLib5.Logging/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntLib5.Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
