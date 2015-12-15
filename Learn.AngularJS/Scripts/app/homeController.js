8b2a72a8b8d982b9ba865bd5709fa7355f6924ff
yuanrui <cqdeveloper@hotmail.com>
2015-12-15 16:41:54 +0800

    add module demo
    add clock to page

--- Learn.AngularJS/Scripts/app/homeController.js
+++ Learn.AngularJS/Scripts/app/homeController.js
@@ -1,9 +1,17 @@
-﻿function MyController($scope) {
+﻿angular.module("myApp", [])
+.controller('MyController', function ($scope, $timeout) {
     var updateClock = function () {
-        $scope.clock = new Date();
+        var dateObj = new Date();
+        $scope.clock = "" + dateObj.getHours() + ":" + dateObj.getMinutes() + ":" + dateObj.getSeconds();
+
+        //        setInterval(function () {
+        //            updateClock();
+        //            //$scope.$apply(updateClock);
+        //        }, 2000);
+        $timeout(function () {
+            updateClock();
+        }, 1000);
     };
-    setInterval(function () {
-        $scope.$apply(updateClock);
-    }, 1000);
+
     updateClock();
-}
+});
