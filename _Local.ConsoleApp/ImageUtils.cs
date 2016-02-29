b26354c33bf25dd937a4caf6536fac006dae6e97
yuanrui <cqdeveloper@hotmail.com>
2016-02-29 17:50:13 +0800

    add _Locl.ConsoleApp project

---
+++ _Local.ConsoleApp/ImageUtils.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Windows.Media.Imaging;
+
+namespace _Local.ConsoleApp
+{
+    public class ImageUtils
+    {
+        private static Bitmap ScaleImage(Bitmap image, double scale)
+        {
+            int newWidth = (int)(image.Width * scale);
+            int newHeight = (int)(image.Height * scale);
+
+            Bitmap result = new Bitmap(image, newWidth, newHeight);
+            result.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                g.DrawImage(image, 0, 0, result.Width, result.Height);
+            }
+            return result;
+        }
+
+        private static void SaveTemporary(Bitmap bmp, MemoryStream ms, int quality)
+        {
+            var qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            var codec = GetImageCodecInfo(bmp.RawFormat);
+            var encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+            bmp.Save(ms, codec, encoderParams);
+        }
+
+        private static ImageCodecInfo GetImageCodecInfo(ImageFormat format)
+        {
+            var codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (var codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+
+            return codecs[0];
+        }
+
+
+        public static byte[] Compress(byte[] byteImageIn, int targetSize)
+        {
+            if (byteImageIn == null)
+            {
+                throw new ArgumentNullException("byteImageIn");
+            }
+
+            if (byteImageIn.Length <= targetSize)
+            {
+                return byteImageIn;
+            }
+
+            using (var ms = new MemoryStream(byteImageIn))
+            {
+                
+                var bmp = (Bitmap)Image.FromStream(ms, false, true);
+
+                while (ms.Length > targetSize)
+                {
+                    var scale = Math.Sqrt((double)targetSize / (double)ms.Length);
+                    ms.SetLength(0);
+                    bmp = ScaleImage(bmp, scale);
+                    //bmp.Save(ms, ImageCodecInfo.GetImageEncoders()[1], new EncoderParameters());
+                    SaveTemporary(bmp, ms, 100);
+                }
+                //bmp.Save("D:\\Test.jpg", ImageFormat.Jpeg);
+                if (bmp != null)
+                {
+                    bmp.Dispose();
+                }
+
+                return ms.ToArray();
+            }
+        }
+
+    }
+}
---
+++ _Local.ConsoleApp/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.Net;
+
+namespace _Local.ConsoleApp
+{
+    class Program
+    {
+        private readonly static byte[] _jpgHeader = { 0xff, 0xd8 };
+        private readonly static byte[] _pngHeader = { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
+        private readonly static byte[] _bmpHeader = { 0x42, 0x4D };
+
+        const string RecordPath = "images.txt";
+        const int ImageMaxSize = 300 * 1024;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                //foreach (var item in ImageCodecInfo.GetImageDecoders())
+                //{
+                //    Console.WriteLine(item.CodecName);
+                //}
+                //foreach (var item in ImageCodecInfo.GetImageEncoders())
+                //{
+                //    Console.WriteLine(item.CodecName);
+                //}
+                var lines = File.ReadAllLines(RecordPath, Encoding.UTF8);
+
+                foreach (var imagePath in lines)
+                {
+                    try
+                    {
+                        var data = DownloadDataByUrl(imagePath, ImageMaxSize);
+                        var format = DecodeImage(data);
+                        Console.WriteLine("path:{0} format:{1}", imagePath, format);
+                    }
+                    catch (Exception innerEx)
+                    {
+                        Console.WriteLine("path:{0} ex:{1}", imagePath, innerEx);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:{0}", ex);
+            }
+
+            Console.WriteLine("\nEnd...");
+            Console.ReadLine();
+        }
+
+        private static byte[] DownloadDataByUrl(string url, int imageMaxSize)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                var imgData = webClient.DownloadData(url);
+                if (imgData.Length < imageMaxSize)
+                {
+                    return imgData;
+                }
+                else
+                {
+                    return ImageUtils.Compress(imgData, imageMaxSize);
+                }
+            }
+        }
+
+
+        public static byte[] GetImageArray(string path)
+        {
+            using (var imgObj = Image.FromFile(path))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    imgObj.Save(ms, imgObj.RawFormat);
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        public static ImageFormat DecodeImage(byte[] imageBuffer)
+        {
+            if (ContainsHeader(imageBuffer, _jpgHeader))
+                return ImageFormat.Jpeg;
+
+            if (ContainsHeader(imageBuffer, _pngHeader))
+                return ImageFormat.Png;
+
+            if (ContainsHeader(imageBuffer, _bmpHeader))
+                return ImageFormat.Bmp;
+
+            return ImageFormat.Bmp;
+        }
+
+        protected static bool ContainsHeader(byte[] buffer, byte[] header)
+        {
+            for (int i = 0; i < header.Length; i += 1)
+            {
+                if (header[i] != buffer[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
