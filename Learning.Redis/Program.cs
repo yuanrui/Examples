ab1fc48ac07d75e7ca843ae8efa5057de2c87f6c
yuanrui <cqdeveloper@hotmail.com>
2016-09-22 17:04:49 +0800

    add learning redis examples

---
+++ Learning.Redis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StackExchange.Redis;
+
+namespace Learning.Redis
+{
+    class Program
+    {
+        protected readonly static ConnectionMultiplexer _redisClient = ConnectionMultiplexer.Connect("localhost");
+        
+        static void Main(string[] args)
+        {
+            var opts = new ConfigurationOptions();
+            opts.ClientName = "Test-Clinet";
+            opts.KeepAlive = 180;
+            opts.Password = "123456";
+            Console.WriteLine(opts.ToString());
+
+            var db = _redisClient.GetDatabase();
+            db.StringSet("Key", Guid.NewGuid().ToString());
+            var value = db.StringGet("Key");
+            Console.WriteLine(value);
+            
+            Console.WriteLine("\nPress Any Key To Exit...");
+            Console.ReadLine();
+        }
+    }
+}
