1c34159bc59d9e42258a3c0f450d75421730e4b0
yuanrui <cqdeveloper@hotmail.com>
2017-10-19 13:46:59 +0800

    add VersionUtils.cs

---
+++ Simple.Common/Utility/VersionUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simple.Common.Utility
+{
+    public static class VersionUtils
+    {
+        public readonly static DateTime VersionDate;
+
+        public readonly static Int64 VersionNumber;
+
+        static VersionUtils()
+        {
+            VersionDate = System.IO.File.GetLastWriteTime(typeof(VersionUtils).Assembly.Location);
+            VersionNumber = Int64.Parse(VersionDate.ToString("yyyyMMddHHmm"));
+        }
+    }
+}
