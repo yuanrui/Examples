4db5f74671e4ce2bf26b7baedf484e4c4b9358de
yuanrui <cqdeveloper@hotmail.com>
2017-10-30 16:45:43 +0800

    add TraceHelper class

---
+++ Simple.Common/Logging/TraceHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Simple.Common.Logging
+{
+    public class TraceHelper
+    {
+        public static void Write(string message)
+        {
+            Trace.Write(message);
+        }
+
+        public static void Write(string format, params object[] args)
+        {
+            Write(string.Format(format, args));
+        }
+
+        public static void WriteLine(string message)
+        {
+            Trace.WriteLine(message);
+        }
+
+        public static void WriteLine(string format, params object[] args)
+        {
+            WriteLine(string.Format(format, args));
+        }
+
+        public static void WriteLineWithCategory(string message, string category)
+        {
+            Trace.WriteLine(message, category);
+        }
+    }
+}
