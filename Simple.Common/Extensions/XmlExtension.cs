40df8a1ac8a6f7416658cbed6810cec948afbcd9
yuanrui <cqdeveloper@hotmail.com>
2018-01-11 15:45:26 +0800

    add XmlExtension

---
+++ Simple.Common/Extensions/XmlExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Simple.Common.Extensions
+{
+    public static class XmlExtension
+    {
+        public static string GetNodeText(this XmlNode node, string xpath)
+        {
+            var singleNode = node.SelectSingleNode(xpath);
+            if (singleNode == null)
+            {
+                return string.Empty;
+            }
+
+            return singleNode.InnerText;
+        }
+
+        public static string GetNodeText(this XmlNode node, string xpath, XmlNamespaceManager nsmgr)
+        {
+            var singleNode = node.SelectSingleNode(xpath, nsmgr);
+            if (singleNode == null)
+            {
+                return string.Empty;
+            }
+
+            return singleNode.InnerText;
+        }
+    }
+}
