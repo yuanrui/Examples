29a1c66bb430e1d6590a7fb2455d681f8cd795bf
yuanrui <cqdeveloper@hotmail.com>
2018-03-29 14:46:02 +0800

    add LocalContext class

---
+++ Simple.Common/Core/LocalContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Web;
+
+namespace Simple.Common
+{
+    public class LocalContext
+    {
+        public static readonly LocalContext Current = new LocalContext();
+
+        private LocalContext()
+        {
+
+        }
+
+        public Object this[Object key]
+        {
+            get
+            {
+                if (HttpContext.Current != null)
+                {
+                    return HttpContext.Current.Items[key];
+                }
+                else
+                {
+                    LocalDataStoreSlot slot = Thread.GetNamedDataSlot(key.ToString());
+                    return Thread.GetData(slot);
+                }
+            }
+            set
+            {
+                if (HttpContext.Current != null)
+                {
+                    HttpContext.Current.Items[key] = value;
+                }
+                else
+                {
+                    LocalDataStoreSlot slot = Thread.GetNamedDataSlot(key.ToString());
+                    Thread.SetData(slot, value);
+                }
+            }
+        }
+    }
+}
