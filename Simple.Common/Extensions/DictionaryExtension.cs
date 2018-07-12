685204d3c8c9d308192a7cbc6f213fe0699b103c
yuanrui <yuanrui@live.cn>
2018-07-12 14:06:24 +0800

    add DictionaryExtension class

---
+++ Simple.Common/Extensions/DictionaryExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simple.Common.Extensions
+{
+    public static class DictionaryExtension
+    {
+        /// <summary>
+        /// try add key value for dictionary, if key not exists then add key value, else do nothing.
+        /// Thread unsafe
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dic"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool TryAdd<TKey, TValue>(this Dictionary<TKey, TValue> dic, TKey key, TValue value)
+        {
+            if (dic == null || dic.ContainsKey(key))
+            {
+                return false;
+            }
+
+            dic.Add(key, value);
+
+            return true;
+        }
+    }
+}
