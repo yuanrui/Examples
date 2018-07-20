4e38d043b810c98b1f99768a330f06b695a2d3af
yuanrui <yuanrui@live.cn>
2018-07-20 16:19:49 +0800

    add tool method for DictionaryExtension

--- Simple.Common/Extensions/DictionaryExtension.cs
+++ Simple.Common/Extensions/DictionaryExtension.cs
@@ -13,20 +13,66 @@ namespace Simple.Common.Extensions
         /// </summary>
         /// <typeparam name="TKey"></typeparam>
         /// <typeparam name="TValue"></typeparam>
-        /// <param name="dic"></param>
+        /// <param name="dict"></param>
         /// <param name="key"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static bool TryAdd<TKey, TValue>(this Dictionary<TKey, TValue> dic, TKey key, TValue value)
+        public static bool TryAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue value)
         {
-            if (dic == null || dic.ContainsKey(key))
+            if (dict == null || dict.ContainsKey(key))
             {
                 return false;
             }
 
-            dic.Add(key, value);
+            dict.Add(key, value);
 
             return true;
+        }
+
+        public static TValue AddOrUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue addValue, Func<TKey, TValue, TValue> updateValueFactory)
+        {
+            if (dict == null)
+            {
+                throw new ArgumentNullException("dict");
+            }
+
+            if (dict.ContainsKey(key))
+            {
+                dict[key] = updateValueFactory(key, addValue);
+            }
+            else
+            {
+                dict[key] = addValue;
+            }
+
+            return dict[key];
+        }
+
+        public static Dictionary<TKey, TValue> Merge<TKey, TValue>(this Dictionary<TKey, TValue> @thisDict, Dictionary<TKey, TValue> @thatDict)
+        {
+            if (@thisDict == null)
+            {
+                throw new ArgumentNullException("thisDict");
+            }
+
+            if (@thatDict == null)
+            {
+                throw new ArgumentNullException("thatDict");
+            }
+
+            var result = new Dictionary<TKey, TValue>(@thisDict.Count + @thatDict.Count);
+
+            foreach (var pair in @thisDict)
+            {
+                result.Add(pair.Key, pair.Value);
+            }
+
+            foreach (var pair in @thatDict)
+            {
+                TryAdd(result, pair.Key, pair.Value);
+            }
+
+            return result;
         }
     }
 }
