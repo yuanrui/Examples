872c26272635ee8111cbc233908997e32e32b417
yuanrui <yuanrui@live.cn>
2018-08-30 16:04:28 +0800

    add custom URL protocol in Windows demo
    add html call app by self-protocol demo code.

---
+++ Study.CustomUrlScheme/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Study.CustomUrlScheme
+{
+    class Program
+    {
+        static string ProcessInput(string s)
+        {
+            // TODO Verify and validate the input 
+            // string as appropriate for your application.
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            //https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/aa767914(v=vs.85)
+            //https://stackoverflow.com/questions/27248542/how-do-i-properly-register-a-protocol-handler-on-windows-8
+            //https://stackoverflow.com/questions/35626050/registering-custom-url-handler-in-c-sharp-on-windows-8
+            const String PROTOCOL = "banana";
+            const String ProgramName = "Study.CustomUrlScheme.exe";
+            Console.WriteLine(ProgramName + " invoked with the following parameters.\r\n");
+            Console.WriteLine("Raw command-line: \n\t" + Environment.CommandLine);
+
+            Console.WriteLine("\n\nArguments:\n");
+            
+            foreach (string s in args)
+            {
+                Console.WriteLine("\t" + ProcessInput(s));
+            }
+            
+            if (args != null && args.Length == 1)
+            {
+                if (args[0] == "-u")
+                {
+                    Console.WriteLine("UnRegister " + ProgramName);
+                    UrlProtocolHelper.UnRegister(PROTOCOL);
+                }
+
+                if (args[0] == "-i")
+                {
+                    Console.WriteLine("Register " + ProgramName);
+                    UrlProtocolHelper.Register(PROTOCOL, Process.GetCurrentProcess().MainModule.FileName, "Banana Test Program");
+                }
+            }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Study.CustomUrlScheme/UrlProtocolHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace Study.CustomUrlScheme
+{
+    public static class UrlProtocolHelper
+    {
+        public static void Register(String protocol, String exePath, String description)
+        {
+            if (String.IsNullOrEmpty(protocol))
+            {
+                throw new ArgumentNullException("protocol");
+            }
+
+            if (String.IsNullOrEmpty(exePath))
+            {
+                throw new ArgumentNullException("exePath");
+            }
+            
+            String protocolValue = description;
+            if (String.IsNullOrEmpty(protocolValue))
+            {
+                protocolValue = protocol + " Protocol";
+            }
+            
+            String rootKey = @"HKEY_CLASSES_ROOT\" + protocol;
+            Registry.SetValue(
+                rootKey,
+                String.Empty,
+                protocolValue,
+                RegistryValueKind.String);
+            Registry.SetValue(
+                rootKey,
+                "URL Protocol",
+                String.Empty,
+                RegistryValueKind.String);
+
+            String command = String.Format("\"{0}\" \"%1\"", exePath);
+            Registry.SetValue(rootKey + @"\shell\open\command", String.Empty, command, RegistryValueKind.String);
+        }
+
+        public static void UnRegister(String protocol)
+        {
+            try
+            {
+                RegistryKey key = Registry.ClassesRoot;
+                key.DeleteSubKeyTree(protocol);
+                key.Close();
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine("delete HKEY_CLASSES_ROOT\\" + protocol + " Exception:" + ex.ToString());
+            }            
+        }
+    }
+}
