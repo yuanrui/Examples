933aa1b8b77c40f70e5b8e9523d78b2a5da9fb92
yuanrui <yuanrui@live.cn>
2018-09-26 10:46:46 +0800

    add Apache.Hadoop.Hbase.Thrift2 lib

---
+++ Learning.Hbase/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Apache.Hadoop.Hbase.Thrift2;
+using Thrift.Transport;
+using Thrift.Protocol;
+
+namespace Learning.Hbase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thriftsocket = new TSocket("192.168.1.40", 8090, 10000);
+            var client = new THBaseService.Client(new TBinaryProtocol(thriftsocket));
+            thriftsocket.Open();
+            var rowKey = new TPut();
+
+            rowKey.Row = Encoding.UTF8.GetBytes(Guid.NewGuid().ToString());
+            var col1 = new TColumnValue(null, null, null);
+        }
+    }
+}
