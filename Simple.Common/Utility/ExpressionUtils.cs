7cdc5ee54f17e3e489493a9712a196393a3edecb
yuanrui <yuanrui@live.cn>
2018-10-16 16:44:54 +0800

    add ExpressionUtils

---
+++ Simple.Common/Utility/ExpressionUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Simple.Common.Utility
+{
+    public class ExpressionUtils
+    {
+        public static MemberInfo GetMemberInfo<T, U>(Expression<Func<T, U>> expression)
+        {
+            var member = expression.Body as MemberExpression;
+            if (member != null)
+                return member.Member;
+
+            throw new ArgumentException("无法通过表达式访问成员", "expression");
+        }
+
+        public static string GetMemberName<T, U>(Expression<Func<T, U>> expression)
+        {
+            return GetMemberInfo(expression).Name;
+        }
+    }
+}
