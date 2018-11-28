ebdf9443b5c2e3805e1e48366e48d19d9749ef5a
yuanrui <yuanrui@live.cn>
2018-11-28 11:54:31 +0800

    Create IdConverter

---
+++ Simple.Common/Text/IdConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simple.Common.Text
+{
+    public static class IdConverter
+    {
+        public static UInt64 ToLong(String id)
+        {
+            var bytes = Convert.FromBase64String(id);
+            Array.Reverse(bytes);
+            return BitConverter.ToUInt64(bytes, 0);
+        }
+
+        public static String ToString(UInt64 id)
+        {
+            var bytes = BitConverter.GetBytes(id);
+            Array.Reverse(bytes);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
