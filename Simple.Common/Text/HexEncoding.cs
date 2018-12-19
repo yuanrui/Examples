9c5cbb00d3121467b11ce97a7a80520e9495ef7b
yuanrui <yuanrui@live.cn>
2018-12-19 15:21:23 +0800

    add ByteSize

--- Simple.Common/Text/HexEncoding.cs
+++ Simple.Common/Text/HexEncoding.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Simple.Common.Text
+﻿namespace Simple.Common.Text
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     public class HexEncoding
     {
         public Byte[] GetBytes(String input)
--- Simple.Common/Text/IdConverter.cs
+++ Simple.Common/Text/IdConverter.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Simple.Common.Text
+﻿namespace Simple.Common.Text
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     public static class IdConverter
     {
         public static UInt64 ToLong(String id)
