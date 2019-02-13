5922b9518d7db4cf4724bb311568e5d55b78d0c9
yuanrui <yuanrui@live.cn>
2019-02-13 16:05:21 +0800

    Update Learning.Redis

--- Learning.Redis/Program.cs
+++ Learning.Redis/Program.cs
@@ -1,44 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using StackExchange.Redis;
 using System.Threading;
+using StackExchange.Redis;
 
 namespace Learning.Redis
 {
     class Program
     {
-        protected readonly static ConnectionMultiplexer _redisClient = ConnectionMultiplexer.Connect("localhost");
+        //protected readonly static ConnectionMultiplexer _redisClient = ConnectionMultiplexer.Connect("localhost");
         
         static void Main(string[] args)
         {
-            var opts = new ConfigurationOptions();
-            opts.ClientName = "Test-Clinet";
-            opts.KeepAlive = 180;
-            opts.Password = "123456";
-            Console.WriteLine(opts.ToString());
+            RedisServerBootmgr.Start();
+            
+            //var opts = new ConfigurationOptions();
+            //opts.ClientName = "Test-Clinet";
+            //opts.KeepAlive = 180;
+            //opts.Password = "123456";
+            //Console.WriteLine(opts.ToString());
+            //Process.GetProcessesByName("redis-server.exe");
+            
+            //var db = _redisClient.GetDatabase();
+            //db.StringSet("Key", Guid.NewGuid().ToString());
+            //var value = db.StringGet("Key");
+            //Console.WriteLine(value);
 
-            var db = _redisClient.GetDatabase();
-            db.StringSet("Key", Guid.NewGuid().ToString());
-            var value = db.StringGet("Key");
-            Console.WriteLine(value);
+            ConnectionMultiplexer _redisClient = ConnectionMultiplexer.Connect("localhost:6399");
+            var inputCmd = Console.ReadLine();
             RedisChannel channel = new RedisChannel("pub/sub", RedisChannel.PatternMode.Auto);
             ISubscriber sub = _redisClient.GetSubscriber();
 
-            sub.Subscribe(channel, (c, v) => {
-                if (! v.IsNull)
-                {
-                    Console.WriteLine(v.ToString());
-                }
-            });
-
-            for (int i = 0; i < 100; i++)
+            if (inputCmd == "sub")
             {
-                var input = i.ToString().PadLeft(3, '0') + "." + Guid.NewGuid().ToString();
-                sub.Publish(channel, input);
+                sub.Subscribe(channel, (c, v) =>
+                {
+                    if (!v.IsNull)
+                    {
+                        Console.WriteLine(v.GetHashCode());
+                        //Console.WriteLine(v.ToString());
+                    }
+                });
             }
-            
+            else
+            {
+                do
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        var input = DateTime.Now.ToString("yyyyMMddHHmmssfff") + "." + i.ToString().PadLeft(3, '0') + "." + Guid.NewGuid().ToString();
+                        for (int j = 0; j < 100; j++)
+                        {
+                            input += Guid.NewGuid();
+                        }
+
+                        if (sub.IsConnected())
+                        {
+                            var number = sub.Publish(channel, input);
+                            Console.WriteLine(number);
+                        }
+                        
+                    }
+                    
+                    Console.WriteLine("Input 'q' to break publish.");
+                    inputCmd = Console.ReadLine();
+                } while (inputCmd != "q");
+            }
+
             Console.WriteLine("\nPress Any Key To Exit...");
             Console.ReadLine();
         }
---
+++ Learning.Redis/RedisServerBootmgr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Learning.Redis
+{
+    public static class RedisServerBootmgr
+    {
+        const String REDIS_SERVER = "redis-server.exe";
+        const String REDIS_CONF = "redis.windows.conf";
+
+        public static void Start()
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.FileName = REDIS_SERVER;
+            startInfo.Arguments = REDIS_CONF;
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            Process.Start(startInfo);
+        }
+    }
+}
