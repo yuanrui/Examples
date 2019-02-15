5e08b21af3e5bdad92d276e35d382dbd576de776
yuanrui <yuanrui@live.cn>
2019-02-15 14:24:40 +0800

    Create SafeRandom class

---
+++ Simple.Common/Utility/SafeRandom.cs
@@ -0,0 +1,50 @@
+﻿namespace Simple.Common.Utility
+{
+    using System;
+    using System.Security.Cryptography;
+
+    public class SafeRandom
+    {
+        private static readonly RandomNumberGenerator GlobalGenerator = RandomNumberGenerator.Create();
+
+        [ThreadStatic]
+        private static Random _random;
+
+        private static Random GetRandom()
+        {
+            if (_random == null)
+            {
+                Byte[] buffer = new Byte[4];
+                GlobalGenerator.GetBytes(buffer);
+                _random = new Random(BitConverter.ToInt32(buffer, 0));
+            }
+
+            return _random;
+        }
+
+        public Int32 Next()
+        {
+            return GetRandom().Next();
+        }
+
+        public Int32 Next(Int32 maxValue)
+        {
+            return GetRandom().Next(maxValue);
+        }
+
+        public Int32 Next(Int32 minValue, Int32 maxValue)
+        {
+            return GetRandom().Next(minValue, maxValue);
+        }
+
+        public void NextBytes(Byte[] buffer)
+        {
+            GetRandom().NextBytes(buffer);
+        }
+
+        public Double NextDouble()
+        {
+            return GetRandom().NextDouble();
+        }
+    }
+}
