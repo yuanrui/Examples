00d6be81deadd4d18f32c8b4ce8e16e9b70f8be4
袁锐 <yuanrui@live.cn>
2019-04-01 17:16:16 +0800

    Update NameValueCollectionExtension.cs

--- Simple.Common/Extensions/NameValueCollectionExtension.cs
+++ Simple.Common/Extensions/NameValueCollectionExtension.cs
@@ -46,5 +46,136 @@ namespace Simple.Common.Extensions
 
             return result;
         }
+
+        internal static T GetValue<T>(NameValueCollection collection, string key, Func<string, T> parseValue) where T : struct
+        {
+            string value = collection[key];
+
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                return parseValue(value);
+            }
+
+            return default(T);
+        }
+
+        internal static T GetValue<T>(NameValueCollection collection, string key, Func<string, T> parseValue, T @defaultValue) where T : struct
+        {
+            string value = collection[key].IsNullOrEmptyReturn(defaultValue.ToString());
+
+            try
+            {
+                return parseValue(value);
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+        public static Byte GetByte(this NameValueCollection collection, string key)
+        {
+            return GetValue(collection, key, Byte.Parse);
+        }
+
+        public static Byte GetByte(this NameValueCollection collection, string key, Byte @defaultValue)
+        {
+            return GetValue(collection, key, Byte.Parse, defaultValue);
+        }
+
+        public static Int16 GetInt16(this NameValueCollection collection, string key)
+        {
+            return GetValue(collection, key, Int16.Parse);
+        }
+
+        public static Int16 GetInt16(this NameValueCollection collection, string key, Int16 @defaultValue)
+        {
+            return GetValue(collection, key, Int16.Parse, defaultValue);
+        }
+
+        public static Int32 GetInt32(this NameValueCollection collection, string key)
+        {
+            return GetValue(collection, key, Int32.Parse);
+        }
+
+        public static Int32 GetInt32(this NameValueCollection collection, string key, Int32 @defaultValue)
+        {
+            return GetValue(collection, key, Int32.Parse, defaultValue);
+        }
+
+        public static Int64 GetInt64(this NameValueCollection collection, string key)
+        {
+            return GetValue(collection, key, Int64.Parse);
+        }
+
+        public static Int64 GetInt64(this NameValueCollection collection, string key, Int64 @defaultValue)
+        {
+            return GetValue(collection, key, Int64.Parse, defaultValue);
+        }
+
+        public static Decimal GetDecimal(this NameValueCollection collection, string key)
+        {
+            return GetValue(collection, key, Decimal.Parse);
+        }
+        public static Decimal GetDecimal(this NameValueCollection collection, string key, Decimal @defaultValue)
+        {
+            return GetValue(collection, key, Decimal.Parse, defaultValue);
+        }
+
+        public static Double GetDouble(this NameValueCollection collection, string key)
+        {
+            return GetValue(collection, key, Double.Parse);
+        }
+
+        public static Double GetDouble(this NameValueCollection collection, string key, Double @defaultValue)
+        {
+            return GetValue(collection, key, Double.Parse, defaultValue);
+        }
+
+        public static Single GetSingle(this NameValueCollection collection, string key)
+        {
+            return GetValue(collection, key, Single.Parse);
+        }
+
+        public static Single GetSingle(this NameValueCollection collection, string key, Single @defaultValue)
+        {
+            return GetValue(collection, key, Single.Parse, defaultValue);
+        }
+
+        public static Boolean GetBoolean(this NameValueCollection collection, string key)
+        {
+            return GetValue(collection, key, Boolean.Parse);
+        }
+
+        public static Boolean GetBoolean(this NameValueCollection collection, string key, Boolean @defaultValue)
+        {
+            return GetValue(collection, key, Boolean.Parse, defaultValue);
+        }
+
+        public static DateTime GetDateTime(this NameValueCollection collection, string key)
+        {
+            return GetValue(collection, key, DateTime.Parse);
+        }
+
+        public static DateTime GetDateTime(this NameValueCollection collection, string key, DateTime @defaultValue)
+        {
+            return GetValue(collection, key, DateTime.Parse, defaultValue);
+        }
+
+        public static String GetString(this NameValueCollection collection, string key, String @defaultValue)
+        {
+            if (collection == null)
+            {
+                return defaultValue;
+            }
+
+            var value = collection[key];
+            if (String.IsNullOrWhiteSpace(value))
+            {
+                return defaultValue;
+            }
+
+            return value;
+        }
     }
 }
