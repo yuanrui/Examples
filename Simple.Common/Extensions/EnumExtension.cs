0f8be3c070a68fd61587be6dc942ee42cc68169b
YuanRui <yuanrui@live.cn>
2019-08-09 14:04:05 +0800

    Create EnumExtension.cs

---
+++ Simple.Common/Extensions/EnumExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simple.Common.Extensions
+{
+    public static class EnumExtension
+    {
+        public static bool HasFlag(this Enum enumRef, Enum flag)
+        {
+            long value = Convert.ToInt64(enumRef);
+            long flagVal = Convert.ToInt64(flag);
+
+            return (value & flagVal) == flagVal;
+        }
+    }
+}
