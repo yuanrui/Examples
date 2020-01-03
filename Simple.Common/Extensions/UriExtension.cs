29c0310d2a554af84cd8b0c991f3035384204104
YuanRui <yuanrui@live.cn>
2020-01-03 16:22:57 +0800

    Create UriExtension.cs

---
+++ Simple.Common/Extensions/UriExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simple.Common.Extensions
+{
+    public static class UriExtension
+    {
+        public static string GetBaseUrl(this Uri uri)
+        {
+            return uri.Scheme + "://" + uri.Host + ":" + uri.Port;
+        }
+    }
+}
